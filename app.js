78e07952e801a1cdfcc58daaf5ac81cbc9f0d137
habibul.hasan.hira <habibul.hasan.hira@gmail.com>
2017-09-24 16:12:35 +0600

    added angular 1 for action

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app=express();
 const port=5000;
+app.use(express.static('public'));
+app.use(express.static('src/views'));
 app.listen(port,function(err){
 
     console.log('Running the server on port '+port);
