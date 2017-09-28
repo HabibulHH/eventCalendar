1c948f38835b9f32dda1caaddc6c56dbde8240d8
habibul.hasan.hira <habibul.hasan.hira@gmail.com>
2017-09-28 13:16:24 +0600

    saving event bug fixed, create update read implemented

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
-var express = require('express');
-var app=express();
+const express = require('express');
+const app=express();
 const port=5000;
 app.use(express.static('public'));
 app.use(express.static('src/views'));
-var bodyParser=require('body-parser');
-var {mongoose}=require('./server/DAL/mongoose');
- var ObjectID = require('mongodb').ObjectID;
+const bodyParser=require('body-parser');
+const {mongoose}=require('./server/DAL/mongoose');
+var ObjectID = require('mongodb').ObjectID;
 var {Event}=require('./server/MODEL/event');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const ObjectId = mongoose.mongo.ObjectId;
-app.post('/event',(req,res)=>{
- 
-    console.log(req.body);
-    
-});
 
-//
+
 
 app.listen(port,function(err){
 
@@ -44,45 +39,47 @@ app.post('/home/SaveEvent',function(req,res){
         IsFullDay:req.body.IsFullDay,
         Description:req.body.Description
     });
-if(req.body.EventID!==null||req.body.EventID!==undefined)
-    {
-      
-    console.log(JSON.stringify(event));
-    console.log(event.id);
-    var id=new ObjectID(event._id);
-    
-    //console.log(id+"   "+mongoose.Types.ObjectId(id.toHexString()));
-        Event.findById(id,function (err, tank) {
-        
-            console.log(JSON.stringify(tank));
-        // console.log(JSON.stringify(tank));
-        tank.Description=event.Description;
-        tank.IsFullDay=event.IsFullDay;
-        tank.Title=event.Title;
-        tank.StartAt=event.StartAt;
-        tank.EndAt=event.EndAt;
 
-        tank.save(function (err, updatedTank) {
-            if (err) return handleError(err);
-            res.send(updatedTank);
-            });
+      
+    //console.log(typeof(req.body.e)===number)
+        var id=new ObjectID(event._id);
+    
+    
+        Event.findById(id,function (err, tank) 
+        {
+
+        
+            if(tank!=null)
+                {
+                            tank.Description=event.Description;
+                            tank.IsFullDay=event.IsFullDay;
+                            tank.Title=event.Title;
+                            tank.StartAt=event.StartAt;
+                            tank.EndAt=event.EndAt;
+
+                            tank.save(function (err, updatedTank) {
+                                if (err) console.log("Can not update data");
+                                res.json(updatedTank);
+                                });
+                }
+            
+                else{
+                        
+                        var SaveEvent= new Event({
+                            Title:req.body.Title,
+                            StartAt:req.body.StartAt,
+                            EndAt:req.body.EndAt,
+                            IsFullDay:req.body.IsFullDay,
+                            Description:req.body.Description
+                        });
+
+                            SaveEvent.save().then((doc)=>{
+                            res.json(doc); 
+                            },(e)=>{
+                            res.status(400).send(e)
+                            console.log('can not save');
+                            })
+                }
          });
 
-    
-    }
-        else{
-           event.save().then((doc)=>{
-        //res.status(200).send(doc)
-        console.log('Data added');
-          res.json(doc);
-          //res.end();
-
-          
-    },(e)=>{
-        res.status(400).send(e)
-        console.log('can not save')
-    })
-
-        }
-   
 });
